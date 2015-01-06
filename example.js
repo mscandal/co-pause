43f9e359bf998d59527886202d16b308260e6824
Matt Scandalis <scandalis.matthew@gmail.com>
2015-01-06 13:03:03 -0500

    updated git info, and example. added readme

--- example.js
+++ example.js
@@ -1,12 +1,14 @@
 var co = require('co');
 var queue = require('./index')();
+var go = queue.go;
+var pause = queue.pause;
 
 co(function*() {
 	console.log('Starting');
-	var a = yield queue.pause();
+	var a = yield pause();
 	console.log(a);
 }).catch(console.log);
 
 setInterval(function() {
-	queue.go('Done');
+	go('Done');
 }, 5*1000);
